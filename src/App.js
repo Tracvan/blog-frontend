55caac7d23208f5a2cdd11351ce7a10cb9b4abe9
khoa1812 <khoatrinh2909@gmail.com>
2024-07-02 16:01:25 +0700

    register

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
-import './App.css';
+import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Post from './components/post/Post';
 import Master from './pages/Master/Master';
+import RegisterForm from './components/register/RegisterForm';
 
 function App() {
-  return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path={"/"} element={<Master />}>
-            <Route path={"/posts"} element={<Post />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Master />} />
+                <Route path="/posts" element={<Post />} />
+                <Route path="/register" element={<RegisterForm />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
