79f5f5cc63baafdb4a0d459c9e2bfc3b17555e4f
DuoMinhHieu <dmh6774@gmail.com>
2024-07-02 16:34:28 +0700

    tao form login

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import {BrowserRouter, Route, Router, Routes} from 'react-router-dom';
 import Post from './components/post/Post';
 import Master from './pages/Master/Master';
+import LoginForm from "./components/LoginForm/LoginForm";
+
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path={"/"} element={<Master />}>
-            <Route path={"/posts"} element={<Post />} />
+            <Route path="/login" element={<LoginForm/>}/>
+            <Route path={"/"} element={<Master />}>
+                <Route path={"/posts"} element={<Post />} />
           </Route>
         </Routes>
       </BrowserRouter>
